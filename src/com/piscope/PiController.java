ab16cf7095b61c5236148260d092cd99765a9a09
Prathyush <kingspprathyush@gmail.com>
2015-01-27 19:43:39 +0530

    Initialized Bottom View with Measured Values and Instructions

--- src/com/piscope/PiController.java
+++ src/com/piscope/PiController.java
@@ -2,11 +2,15 @@ package com.piscope;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import org.gillius.jfxutils.JFXUtil;
 import org.gillius.jfxutils.chart.ChartPanManager;
 import org.gillius.jfxutils.chart.JFXChartUtil;
 import org.gillius.jfxutils.chart.StableTicksAxis;
+
+
 
 public class PiController {
 	
@@ -17,6 +21,11 @@ public class PiController {
 	@FXML private StableTicksAxis xAxis;
 	@FXML private StableTicksAxis yAxis;
 	
+	//Bottom label references
+	@FXML private Label xyValues;
+	@FXML private Label measurement;
+	@FXML private Label instructions;
 	
-	
+	//Bottom List reference
+	@FXML private ListView<String> instructionList;
 }
