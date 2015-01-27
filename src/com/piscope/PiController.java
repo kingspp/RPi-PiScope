eea0a779ec401feaecc451a9b8208226928f08e5
Prathyush <kingspprathyush@gmail.com>
2015-01-27 19:34:41 +0530

    Added JFXUtils package

--- src/com/piscope/PiController.java
+++ src/com/piscope/PiController.java
@@ -1,5 +1,22 @@
 package com.piscope;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+
+import org.gillius.jfxutils.JFXUtil;
+import org.gillius.jfxutils.chart.ChartPanManager;
+import org.gillius.jfxutils.chart.JFXChartUtil;
+import org.gillius.jfxutils.chart.StableTicksAxis;
+
 public class PiController {
+	
+	//LineChart Reference
+	@FXML private LineChart<Number, Number> PiChart;
+	
+	//Axis Reference
+	@FXML private StableTicksAxis xAxis;
+	@FXML private StableTicksAxis yAxis;
+	
+	
 	
 }
