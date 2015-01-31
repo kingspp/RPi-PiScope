d22a5d0b80486c2c9f63494063ff3eb9289e7b51
Prathyush <kingspprathyush@gmail.com>
2015-02-01 01:25:18 +0530

    Added dialog with Icon - About

--- src/com/piscope/PiController.java
+++ src/com/piscope/PiController.java
@@ -9,20 +9,30 @@ import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 
+import javafx.animation.PauseTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import org.gillius.jfxutils.JFXUtil;
 import org.gillius.jfxutils.chart.ChartPanManager;
 import org.gillius.jfxutils.chart.JFXChartUtil;
 import org.gillius.jfxutils.chart.StableTicksAxis;
+
+
 
 
 
@@ -57,6 +67,10 @@ public class PiController {
 	String In9="* Note the the colour of the line changes to red when the line is selected";
 	String In10="* Delete the line by hovering on it and clicking Secondary mouse key";
 	
+
+	//Dialog Variable in seconds
+	double dialogTimeout=10;
+	
 	//Initialization function
 	@FXML
 	void initialize() {
@@ -86,5 +100,27 @@ public class PiController {
 	    }
 	}
 	
+	
+	//This snippet is used to build Dialog
+	@FXML
+	void dialogBuild() throws InterruptedException
+	{
+		
+		 final Stage dialog = new Stage();
+         dialog.initModality(Modality.APPLICATION_MODAL);         
+         VBox dialogVbox = new VBox(20);
+         dialogVbox.getChildren().add(new Text("\t\t\t\tAbout PiScope v1.0"));
+         dialogVbox.getChildren().add(new Text("\tTeam:\n\t Prathyush\n\t Shshikiran\n\t Vinay\n\t Amaraprabhu"));
+         dialogVbox.getChildren().add(new Text("\tProject Guide    :\t Prof MG Srinivas\n\tTechnical Support:\t Chandra Prasad Sir"));         
+         Scene dialogScene = new Scene(dialogVbox, 400, 300);
+         dialog.getIcons().add( new Image( PiMain.class.getResourceAsStream( "icon.png" )));          
+         dialog.setScene(dialogScene);
+         dialog.show();         
+         PauseTransition pause = new PauseTransition(Duration.seconds(dialogTimeout));
+         pause.setOnFinished(e -> dialog.hide());
+         pause.play();
+         
+		
+	}
 	
 }
