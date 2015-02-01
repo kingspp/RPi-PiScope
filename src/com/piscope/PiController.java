64679c2ff672ee1a95d48523034e5c758191a96c
Prathyush <kingspprathyush@gmail.com>
2015-02-01 08:46:29 +0530

    Added Timer

--- src/com/piscope/PiController.java
+++ src/com/piscope/PiController.java
@@ -33,99 +33,117 @@ import org.gillius.jfxutils.chart.ChartPanManager;
 import org.gillius.jfxutils.chart.JFXChartUtil;
 import org.gillius.jfxutils.chart.StableTicksAxis;
 
-
-
 public class PiController {
-	
-	//LineChart Reference
-	@FXML private LineChart<Number, Number> PiChart;
-	
-	//XYChart Series declaration
-	private XYChart.Series<Number, Number> PiSeries;
-	
-	//Axis Reference
-	@FXML private StableTicksAxis xAxis;
-	@FXML private StableTicksAxis yAxis;
-	
-	//Bottom label references
-	@FXML private Label xyValues;
-	@FXML private Label measurement;
-	@FXML private Label instructions;
-	
-	//Bottom List reference
-	@FXML private ListView<String> instructionList;
-	
-	//Instruction Strings
-	String In1="* Use Start/Stop button to Start/Stop Waveforms";
-	String In2="* Use auto range button to set Auto";
-	String In3="* Scroll Mouse to Zoom In/Out";
-	String In4="* Select the part of waveform using Primary Mouse key";
-	String In5="* Use seconday Mouse key to shift the waveform";
-	String In6="* Double click to set statting position for a line";
-	String In7="* Double click again to end the line";
-	String In8="* Hover on the line and drag it using left click";
-	String In9="* Note the the colour of the line changes to red when the line is selected";
-	String In10="* Delete the line by hovering on it and clicking Secondary mouse key";	
 
-	//Dialog Variable in seconds
-	double dialogTimeout=10;
-	
-	
-	
-	//Initialization function
+	// LineChart Reference
+	@FXML
+	private LineChart<Number, Number> PiChart;
+
+	// XYChart Series declaration
+	private XYChart.Series<Number, Number> PiSeries;
+
+	// Axis Reference
+	@FXML
+	private StableTicksAxis xAxis;
+	@FXML
+	private StableTicksAxis yAxis;
+
+	// Bottom label references
+	@FXML
+	private Label xyValues;
+	@FXML
+	private Label measurement;
+	@FXML
+	private Label instructions;
+
+	// Bottom List reference
+	@FXML
+	private ListView<String> instructionList;
+
+	// Instruction Strings
+	String In1 = "* Use Start/Stop button to Start/Stop Waveforms";
+	String In2 = "* Use auto range button to set Auto";
+	String In3 = "* Scroll Mouse to Zoom In/Out";
+	String In4 = "* Select the part of waveform using Primary Mouse key";
+	String In5 = "* Use seconday Mouse key to shift the waveform";
+	String In6 = "* Double click to set statting position for a line";
+	String In7 = "* Double click again to end the line";
+	String In8 = "* Hover on the line and drag it using left click";
+	String In9 = "* Note the the colour of the line changes to red when the line is selected";
+	String In10 = "* Delete the line by hovering on it and clicking Secondary mouse key";
+
+	// Dialog Variable in seconds
+	double dialogTimeout = 10;
+
+	// Timer Variables
+	private long startTime;
+
+	// Initialization function
 	@FXML
 	void initialize() {
+
+		ObservableList<String> items = FXCollections.observableArrayList(In1,
+				In2, In3, In4, In5, In6, In7, In8, In9, In10);
+		instructionList.setItems(items);
+
+		// Set Chart Properties
+		xAxis.setForceZeroInRange(false);
 		
-		ObservableList<String> items =FXCollections.observableArrayList (
-			    In1, In2, In3, In4, In5, In6, In7, In8, In9, In10);
-			instructionList.setItems(items);
-			
-			//Set Labels
-			xAxis.setLabel("Time (ms)");
-			yAxis.setLabel("Voltage (V)");
-			
-			//Add series
-			PiSeries = new XYChart.Series<Number, Number>();
-			PiSeries.setName( "Data" );
-			PiChart.getData().add( PiSeries );
+		//Get Current Time
+		startTime = System.currentTimeMillis();
+
+		// Set Labels
+		xAxis.setLabel("Time (ms)");
+		yAxis.setLabel("Voltage (V)");
+
+		// Add series
+		PiSeries = new XYChart.Series<Number, Number>();
+		PiSeries.setName("Data");
+		PiChart.getData().add(PiSeries);
 	}
-	
-	//Save Rendering
+
+	// Save Rendering
 	@FXML
 	public void saveAsPng() {
-		String timeStamp = new SimpleDateFormat("HHmmss_ddMMyyyy").format(Calendar.getInstance().getTime());		
-		PiChart.setAnimated(false);				
+		String timeStamp = new SimpleDateFormat("HHmmss_ddMMyyyy")
+				.format(Calendar.getInstance().getTime());
+		PiChart.setAnimated(false);
 		System.out.println("Saving . . .");
-	    WritableImage image = PiChart.snapshot(new SnapshotParameters(), null);	    
-	    File file = new File("chart"+timeStamp+".png");
-	    try {
-	    	ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
-	    } catch (IOException e) {
-	    	 Logger.getLogger(PiController.class.getName()).log(Level.SEVERE, null, e);
-	    	 System.out.println("Error");
-	    }
+		WritableImage image = PiChart.snapshot(new SnapshotParameters(), null);
+		File file = new File("chart" + timeStamp + ".png");
+		try {
+			ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+		} catch (IOException e) {
+			Logger.getLogger(PiController.class.getName()).log(Level.SEVERE,
+					null, e);
+			System.out.println("Error");
+		}
 	}
-	
-	
-	//This snippet is used to build Dialog
+
+	// This snippet is used to build Dialog
 	@FXML
-	void dialogBuild() throws InterruptedException
-	{		
-		 final Stage dialog = new Stage();
-         dialog.initModality(Modality.APPLICATION_MODAL);         
-         VBox dialogVbox = new VBox(20);
-         dialogVbox.getChildren().add(new Text("\t\t\t\tAbout PiScope v1.5"));
-         dialogVbox.getChildren().add(new Text("\tTeam:\n\t Prathyush\n\t Shshikiran\n\t Vinay\n\t Amaraprabhu"));
-         dialogVbox.getChildren().add(new Text("\tProject Guide    :\t Prof MG Srinivas\n\tTechnical Support:\t Chandra Prasad Sir"));         
-         Scene dialogScene = new Scene(dialogVbox, 400, 300);
-         dialog.getIcons().add( new Image( PiMain.class.getResourceAsStream( "icon.png" )));          
-         dialog.setScene(dialogScene);
-         dialog.show();         
-         PauseTransition pause = new PauseTransition(Duration.seconds(dialogTimeout));
-         pause.setOnFinished(e -> dialog.hide());
-         pause.play();         
-		
+	void dialogBuild() throws InterruptedException {
+		final Stage dialog = new Stage();
+		dialog.initModality(Modality.APPLICATION_MODAL);
+		VBox dialogVbox = new VBox(20);
+		dialogVbox.getChildren().add(new Text("\t\t\t\tAbout PiScope v1.5"));
+		dialogVbox
+				.getChildren()
+				.add(new Text(
+						"\tTeam:\n\t Prathyush\n\t Shshikiran\n\t Vinay\n\t Amaraprabhu"));
+		dialogVbox
+				.getChildren()
+				.add(new Text(
+						"\tProject Guide    :\t Prof MG Srinivas\n\tTechnical Support:\t Chandra Prasad Sir"));
+		Scene dialogScene = new Scene(dialogVbox, 400, 300);
+		dialog.getIcons().add(
+				new Image(PiMain.class.getResourceAsStream("icon.png")));
+		dialog.setScene(dialogScene);
+		dialog.show();
+		PauseTransition pause = new PauseTransition(
+				Duration.seconds(dialogTimeout));
+		pause.setOnFinished(e -> dialog.hide());
+		pause.play();
 	}
-	
-	
+
 }
