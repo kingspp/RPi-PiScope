77d674411db87e51954de3308beb3f69e3b25d90
kingspp <kingspprathyush@gmail.com>
2015-04-11 23:15:21 +0530

    Added fft transform method in PiComp

--- src/com/piscope/PiComp.java
+++ src/com/piscope/PiComp.java
@@ -1,5 +1,22 @@
 package com.piscope;
 
 public class PiComp {
+	
+	/* 
+	 * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
+	 * The vector can have any length. This is a wrapper function.
+	 */
+	public static void transform(double[] real, double[] imag) {
+		if (real.length != imag.length)
+			throw new IllegalArgumentException("Mismatched lengths");
+		
+		int n = real.length;
+		if (n == 0)
+			return;
+		else if ((n & (n - 1)) == 0)  // Is power of 2
+			transformRadix2(real, imag);
+		else  // More complicated algorithm for arbitrary sizes
+			transformBluestein(real, imag);
+	}
 
 }
